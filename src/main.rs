b0929e3a5368ec6dc6c89bd9ba18983136dab475
au583665 <klemmensen11@gmail.com>
2023-04-26 04:39:25 +0200

    reads arguments parsed by rparameters in cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{env, cmp::Ordering};
+use colored::Colorize;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // Check if the program was called with the correct number of arguments, stop if not
+    match args.len().cmp(&2) {
+        Ordering::Less => {
+            println!("{} Cargo run was called with no arguments", "Error:".red());
+            return;
+        },
+        Ordering::Equal => (),
+        Ordering::Greater => {
+            println!("{} Cargo run was called with too many arguments", "Error:".red());
+            return;
+        }
+    }
+
+    // The first argument is the name of the program
+    let program_name = &args[0];
+
+    // The second argument is the parameter
+    let parameters = String::from(&args[1]);
+
+    println!("Program name: {}", program_name);
+    println!("Parameters: {:?}", parameters);
 }
