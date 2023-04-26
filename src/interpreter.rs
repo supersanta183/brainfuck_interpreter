cf911a9174f926c2d3613d18f5b1d63ae673c9d7
au583665 <klemmensen11@gmail.com>
2023-04-26 05:29:13 +0200

    added eval function

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,18 +1,24 @@
-use std::cmp::Ordering;
-use colored::Colorize;
 
 pub struct Interpreter {}
 
 impl Interpreter {
+    //initializes a new interpreter
     pub fn new() -> Interpreter {
         Interpreter {}
     }
 
+    //function that interprets a string and returns a result. OK if the string is valid brainfuck and Err otherwise.
     pub fn interpret(&self, source: String) -> Result<String, String> {
-        let result = match source.as_str() {
-            "Hello" => String::from("Hello World!"),
-            _ => return Err(String::from("Unknown command"))
+        let result = match self.eval(source) {
+            Ok(_) => todo!(),
+            Err(_) => todo!(),
         };
+
         Ok(result)
+    }
+
+    //evaluates the source string. OK returns the interpretted value and Err returns an error code.
+    fn eval(&self, source: String) -> Result<String, i64> {
+        todo!()
     }
 }
